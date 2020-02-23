5a78f93963a80abdfb9ceb30f85b3506930631e5
Ricardo2Silva <ricardojonasdasilva60@gmail.com>
2020-02-23 08:52:00 -0300

    acessando banco de dados

--- app/routes/listaContatos.routes.js
+++ app/routes/listaContatos.routes.js
@@ -2,15 +2,29 @@
 module.exports = (app) => {
     
     app.get('/lista',(req,res)=>{
-        res.send('lista telefonica');
+       
+     let mysql = require('mysql');
+    
+        const dadosConexao = {
+                host: 'localhost',
+                user: 'root',
+                password: 'root',
+                database: 'contatos'
+            }
 
-
-    });
-
-    app.get('/home', (req, res) => {
-        res.send('<html><body><h1>Bem Vindo !</h1></body></html>')
-    });
+            let connection = mysql.createConnection(dadosConexao);
         
+            connection.query('select * from contato', (err, resultadoQuery) => {
+              
+                if (err) console.log('ocorreu um erro na listagem dos contatos:', err);
+    
+                res.send(resultadoQuery);
+            });
+    
+            connection.end();    
+        
+    
+    });      
 }
 
 
