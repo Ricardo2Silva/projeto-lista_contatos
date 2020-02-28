0e68d0dfdbd4846f584c4d372b223a8f2894f96c
Ricardo2Silva <ricardojonasdasilva60@gmail.com>
2020-02-28 14:09:48 -0300

    subindo insert

---
+++ insert tipo contato.sql
@@ -0,0 +1,4 @@
+insert into tipo_contato(descricao) values ('email');
+insert into tipo_contato(descricao) values ('celular');
+insert into tipo_contato(descricao) values ('telefone residencial');
+insert into tipo_contato(descricao) values ('telefone comercial');
